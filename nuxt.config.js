426773c624f8b3827322d0b87d949ca3e40e9778
jay <jayforgood@gmail.com>
2021-12-30 08:26:03 +0900

    mod config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,9 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   target: 'static',
+  router: {
+    base: '/jhleedlcns/nuxt-cs-pages/',
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - nuxt-cs-pages',
