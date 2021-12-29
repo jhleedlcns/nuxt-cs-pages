74e76ae26adc9c0f5ffa61eda1b23c35bce1ee2d
jay <jayforgood@gmail.com>
2021-12-30 08:28:19 +0900

    add dist

---
+++ dist/_nuxt/static/1640819687/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1640819687",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5d996ddc"}}}}(null,"\u002F"));
